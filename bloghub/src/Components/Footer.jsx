29897516a06e98a4094fd7a53b6d2f606cf0982b
Rana <ranamohamad215@gmail.com>
2024-06-29 18:37:21 +0300

    created footer.

--- bloghub/src/Components/Footer.jsx
+++ bloghub/src/Components/Footer.jsx
@@ -2,10 +2,14 @@
 
 function Footer() {
     return (
-        <div className=' bg-black py-4 px-6 text-md font-semibold h-[70px] flex justify-between    text-white '>
+        <div className=" flex-col">
+            <div className=" bg-white h-2">
+        </div>
+            <div className=' bg-black py-4 px-6 text-md font-semibold h-[70px] flex justify-between    text-white '>
             <p>Created by Rana </p>
             <p>Presented to: Slash \. </p>
             <p>Icon by Freepik</p>
+        </div>
         </div>
     )
 }
