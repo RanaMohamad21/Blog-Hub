6849d9d87c6a6bb35c8c0760c3cead1105df9948
Rana <ranamohamad215@gmail.com>
2024-08-16 17:30:04 +0300

    Added width and padding to the post card.

--- src/Components/postsCard.jsx
+++ src/Components/postsCard.jsx
@@ -9,11 +9,11 @@ function PostsCard({post}) {
     }
 
     return (
-        <div className=' w-80 h-[280px] flex-col justify-center bg-white text-black m-4 py-4 px-6 rounded-lg'>
+        <div className=' w-90  flex-col justify-center bg-white text-black m-4 py-4 px-6 rounded-lg pb-10'>
           
-           <p className=' text-center text-[21px] font-bold capitalize '>{trancateText(title,4)}</p>
+           <p className=' text-center text-[21px] font-bold capitalize ' title={title}>{trancateText(title,4)}</p>
             <p className=' font-semibold my-1 text-[17px]'>Author: {"Author name"}</p>
-            <p className=' text-[16px]'>{trancateText(body,30)} <span className=' text-darkGray font-semibold hover:cursor-pointer'>Read more</span></p>
+            <p className=' text-[16px]'>{trancateText(body,30)} <span className=' text-darkGray font-semibold hover:cursor-pointer hover:border-b-2 hover:border-darkGray'>Read more</span></p>
            
 
             
