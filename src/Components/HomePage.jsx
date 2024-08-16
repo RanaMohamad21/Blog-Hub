575940e4cb3babe5fd017c2e2f54f70e8b8414ff
Rana <ranamohamad215@gmail.com>
2024-08-16 17:28:22 +0300

    Separted the initial data into a BlogList.js file.

--- src/Components/HomePage.jsx
+++ src/Components/HomePage.jsx
@@ -2,22 +2,28 @@ import CreatePost from "./CreatePost";
 import Footer from "./Footer";
 import NavBar from "./NavBar";
 import PostsCard from "./postsCard";
-const posts = [{   title: "sunt aut facere repellat provident occaecati", body: "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto autem sunt rem eveniet architecto autem sunt rem eveniet architectoautem sunt rem eveniet architecto"
-},{   title: "sunt aut facere repellat provident occaecati", body: "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto autem sunt rem eveniet architecto autem sunt rem eveniet architectoautem sunt rem eveniet architecto"
-},{   title: "sunt aut facere repellat provident occaecati", body: "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto autem sunt rem eveniet architecto autem sunt rem eveniet architectoautem sunt rem eveniet architecto"
-}]
+import initialBlogList from "../BlogList";
+import { useState } from "react";
+
 
 function HomePage() {
-  return <div className=" grid grid-rows-[auto,1fr,auto]  min-h-screen">
-            <NavBar/>
-          {/* <div className=" flex items-center justify-center">
-          <div className=" grid md:grid-cols-2 lg:grid-cols-3 gap-2"> {posts.map((post, index) =>{
+  const [blogList, setBlogList] = useState(initialBlogList);
+  return (
+    <div className=" grid grid-rows-[auto,1fr,auto]  min-h-screen">
+      <NavBar />
+      <div className=" flex items-center justify-center">
+          <div className=" grid md:grid-cols-2 lg:grid-cols-3 gap-2"> {initialBlogList.map((post, index) =>{
+                return <PostsCard post={post} key={index} />
+            })}</div>
+          </div>
+          {/* <div className=" grid md:grid-cols-2 lg:grid-cols-3 gap-2"> {posts.map((post, index) =>{
                 return <PostsCard post={post} key={index} />
             })}</div>
           </div> */}
-          <CreatePost/>
-            <Footer/>
-  </div>;
+      {/* <CreatePost /> */}
+      <Footer />
+    </div>
+  );
 }
 
 export default HomePage;
