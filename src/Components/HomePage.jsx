9acb5571d96451f243efb2f45c2fdbe5a898e124
Rana <ranamohamad215@gmail.com>
2024-09-01 23:22:40 +0300

    Finished the home page.

--- src/Components/HomePage.jsx
+++ src/Components/HomePage.jsx
@@ -1,18 +1,15 @@
-import CreatePost from "./CreatePost";
 import Footer from "./Footer";
 import NavBar from "./NavBar";
 import PostsCard from "./postsCard";
-import initialBlogList from "../BlogList";
-import { useState } from "react";
+import PropTypes from 'prop-types';
 
 
-function HomePage() {
-  const [blogList, setBlogList] = useState(initialBlogList);
-  return (
+function HomePage({blogList}) {
+   return (
     <div className=" grid grid-rows-[auto,1fr,auto]  min-h-screen">
       <NavBar />
       <div className=" flex items-center justify-center">
-          <div className=" grid md:grid-cols-2 lg:grid-cols-3 gap-2"> {initialBlogList.map((post, index) =>{
+          <div className=" grid md:grid-cols-2 lg:grid-cols-3 gap-2"> {blogList.map((post, index) =>{
                 return <PostsCard post={post} key={index} />
             })}</div>
           </div>
@@ -25,5 +22,7 @@ function HomePage() {
     </div>
   );
 }
-
+HomePage.propTypes = {
+  blogList: PropTypes.array
+};
 export default HomePage;
