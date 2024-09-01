d5a0b60857064784f8bae5ce2d5768b907321462
Rana <ranamohamad215@gmail.com>
2024-09-01 23:45:25 +0300

    Fixed an error.

--- src/Components/postsCard.jsx
+++ src/Components/postsCard.jsx
@@ -4,24 +4,41 @@ import { Link } from "react-router-dom";
 function PostsCard({ post }) {
   const { title, body, author, id } = post;
   let words = [];
+
+  // A function that converts html into plain text
+  function convertHtmlToPlainText(htmlString) {
+    // Create a new DOMParser instance
+    const parser = new DOMParser();
+
+    // Parse the HTML string into a Document object
+    const doc = parser.parseFromString(htmlString, "text/html");
+    // Extract and return the text content from the document
+    return doc.body.textContent || "";
+  }
+
+  // This function trancates the post body after converting it from html into plain text and returns only the number of words specified by the limit
   function trancateText(text, limit) {
-    words = text.split(" ");
+    words = convertHtmlToPlainText(text).split(" ");
+
     return (
       words.slice(0, limit).join(" ") + (words.length > limit ? " ..." : "")
     );
   }
 
   return (
-    <Link to={`/viewpost/${id}`} state={{post}}>
+    <Link to={`/viewpost/${id}`} state={{ post }}>
       <div className="sm:w-[80%] mx-auto md:w-90  flex-col justify-center bg-white text-black m-4 py-4 px-6 rounded-lg pb-10">
+        {/* Post title */}
         <p
           className=" text-center text-[21px] font-bold capitalize mb-2"
           title={title}
         >
           {title}
         </p>
+        {/* Post Author Name */}
         <p className=" font-semibold my-1 text-[17px]">Author: {author}</p>
-        {/* <p className=' font-semibold my-1 text-[17px]'> {date}</p> */}
+
+        {/* Post body */}
         <p className=" text-[16px]">
           {trancateText(body, 30)}{" "}
           {words.length > 10 && (
