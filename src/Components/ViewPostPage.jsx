61f6694f0722a530b70d54411448630e7a0125c2
Rana <ranamohamad215@gmail.com>
2024-09-01 23:50:27 +0300

    Refactored the functions.

--- src/Components/ViewPostPage.jsx
+++ src/Components/ViewPostPage.jsx
@@ -1,11 +1,12 @@
 import { Link, useLocation } from "react-router-dom";
 import NavBar from "./NavBar";
 import Footer from "./Footer";
-import ReactHtmlParser from 'react-html-parser';
+import ReactHtmlParser from "react-html-parser";
 function ViewPostPage() {
   const location = useLocation();
   const { post } = location.state || {};
- 
+
+  //   Change the format of the date to make the month a text.
   function formateDate(dateString) {
     const date = new Date(dateString);
     const options = { year: "numeric", month: "long", day: "numeric" };
@@ -17,6 +18,7 @@ function ViewPostPage() {
       <NavBar />
       {!post ? (
         <div className=" text-black h-full text-center flex justify-center items-center font-bold  flex-col">
+          {/*  // Handling the case where No post is found */}
           <p className=" text-5xl">Post not found!</p>
           <Link to="/">
             <div className="bg-black text-xl  w-[20rem] mt-6 font-normal capitalize px-4 py-2 rounded-xl hover:shadow-lg text-white">
@@ -31,6 +33,8 @@ function ViewPostPage() {
         </div>
       ) : (
         <div className=" py-8  flex items-center justify-center  ">
+          {/*  // Handking the case where the post is found
+           */}
           <div className="bg-white rounded-2xl h-full p-3 pb-6 min-w-1/2 max-w-[75%] lg:max-w-[50%] px-8 flex flex-col items-center ">
             <p className=" font-bold text-xl py-4">{post.title}</p>{" "}
             <div>
