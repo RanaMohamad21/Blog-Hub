4475d962ee085da30d05221e86c373f6eb2e5308
Rana <ranamohamad215@gmail.com>
2024-09-01 23:50:06 +0300

    Removed unnecessary comments.

--- src/Components/HomePage.jsx
+++ src/Components/HomePage.jsx
@@ -1,28 +1,26 @@
 import Footer from "./Footer";
 import NavBar from "./NavBar";
 import PostsCard from "./postsCard";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
-
-function HomePage({blogList}) {
-   return (
+function HomePage({ blogList }) {
+  return (
     <div className=" grid grid-rows-[auto,1fr,auto]  min-h-screen">
       <NavBar />
       <div className=" flex items-center justify-center">
-          <div className=" grid md:grid-cols-2 lg:grid-cols-3 gap-2"> {blogList.map((post, index) =>{
-                return <PostsCard post={post} key={index} />
-            })}</div>
-          </div>
-          {/* <div className=" grid md:grid-cols-2 lg:grid-cols-3 gap-2"> {posts.map((post, index) =>{
-                return <PostsCard post={post} key={index} />
-            })}</div>
-          </div> */}
-      {/* <CreatePost /> */}
+        <div className=" grid md:grid-cols-2 lg:grid-cols-3 gap-2">
+          {" "}
+          {blogList.map((post, index) => {
+            return <PostsCard post={post} key={index} />;
+          })}
+        </div>
+      </div>
+
       <Footer />
     </div>
   );
 }
 HomePage.propTypes = {
-  blogList: PropTypes.array
+  blogList: PropTypes.array,
 };
 export default HomePage;
