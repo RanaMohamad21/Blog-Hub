d3009812c952254cf14c697a589941a448320dc4
Rana <ranamohamad215@gmail.com>
2024-09-01 23:22:54 +0300

    Set the links in the nav bar.

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,16 +1,18 @@
 import PropTypes from 'prop-types';
 import penIcon from "../assets/pen-white.png";
+import { Link } from 'react-router-dom';
 function NavBar({isCreatePage = false}) {
     return (
        <div className=' flex-col'>
          <div className=' bg-black h-[70px] flex justify-between    text-white '>
                
-            <div className=' p-4 flex hover:cursor-pointer  font-bold text-5xl'>
+           <Link to="/">
+           <div className=' p-4 flex hover:cursor-pointer  font-bold text-5xl'>
                 <div className=' m-2'> <img src={penIcon} width={"20"} height={"20"}/></div><h1 >Blog Hub</h1>
-            </div>
-            {!isCreatePage?<div className='  text-white hover:cursor-pointer   p-4' >
+            </div></Link>
+            {!isCreatePage?<div className='  text-white hover:cursor-pointer  flex items-center pr-5' >
                 <p className=' hover:border-b-2 hover:border-b-white '>
-                Create Post
+                <Link to={'/create'}>Create Post</Link>
                 </p>
                 </div>:""}
             
