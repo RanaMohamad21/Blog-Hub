f85f38fa320c4563cf36c238769b7aba0173c916
Rana <ranamohamad215@gmail.com>
2024-10-04 21:56:10 +0300

    Added a style prop.

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -1,8 +1,8 @@
 import PropTypes from "prop-types";
 
-function Button({ handleClick, buttonTitle }) {
+function Button({ handleClick, buttonTitle, style ="" }) {
   return (
-    <div>
+    <div className= { ` ${style} ` } >
       <button
         onClick={handleClick}
         className=" text-white capitalize hover:shadow-lg mt-4 rounded-xl bg-black w-full py-2 "
